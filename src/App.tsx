89678839a5b54f4b1ed5a0219ec9ae9ace04e703
kylemit <Kylemit@gmail.com>
2022-02-27 12:55:22 -0500

    convert to react

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <h1 id="title">Coming Soon 2022...</h1>
+        <p>
+          View last years game at:<br/>
+          <a href="https://2021.birthday.money/">2021.birthday.money</a>
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
